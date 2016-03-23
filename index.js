1f61c9cb89d714bfde41c8325056cbd60e72e062
luke <luke.edwards05@gmail.com>
2016-03-23 15:01:21 -0700

    rename 'key' param to 'val' for body keyName value to enter. Add more methods

--- index.js
+++ index.js
@@ -33,11 +33,63 @@ API.prototype.request = function(endpt, opts) {
   return got[act](endpt, opts).catch(err => {
     console.log('error!', err);
   }).then(data =>
-    (opts._key) ? data.body[opts._key] : data.body);
+    (opts.val) ? data.body[opts.val] : data.body);
 };
 
+/**
+ * Get the User's Account Info
+ * @return {Promise}
+ */
 API.prototype.getUserInfo = function () {
-  return this.request('account', {_key: 'account'});
+  const val = 'account';
+  return this.request(val, {val});
+};
+
+API.prototype.listActions = function () {
+  const val = 'actions';
+  return this.request(val, {val});
+};
+
+API.prototype.getAction = function (id) {
+  return this.request(`actions/${id}`, {val: 'action'});
+};
+
+API.prototype.listDomains = function () {
+  const val = 'domains';
+  return this.request(val, {val});
+};
+
+API.prototype.listDroplets = function () {
+  const val = 'droplets';
+  return this.request(val, {val});
+};
+
+API.prototype.getDroplet = function (id) {
+  return this.request(`droplets/${id}`, {val: 'droplet'});
+};
+
+API.prototype.listDropletKernels = function (id) {
+  const val = 'kernels';
+  return this.request(`droplets/${id}/${val}`, {val});
+};
+
+API.prototype.listDropletSnapshots = function (id) {
+  const val = 'snapshots';
+  return this.request(`droplets/${id}/${val}`, {val});
+};
+
+API.prototype.listDropletBackups = function (id) {
+  const val = 'backups';
+  return this.request(`droplets/${id}/${val}`, {val});
+};
+
+API.prototype.listDropletActions = function (id) {
+  const val = 'actions';
+  return this.request(`droplets/${id}/${val}`, {val});
+};
+
+API.prototype.getDropletAction = function (dId, aId) {
+  return this.request(`droplets/${dId}/actions/${aId}`, {val: 'action'});
 };
 
 module.exports = API;
