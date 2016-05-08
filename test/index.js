841ce0ef005548b6e9755bb2c3d597c5428f394b
luke <luke.edwards05@gmail.com>
2016-05-08 13:59:21 -0700

    just use a FAKEID for everything, alternate throws/notthrows as expected

--- test/index.js
+++ test/index.js
@@ -2,6 +2,8 @@ import test from 'ava';
 const DoV2 = require('../');
 const API = new DoV2({token: process.env.DOTOKEN});
 
+const FAKEID = 'aaa';
+
 test('throw without a `token`', t => {
 	t.throws(() => new DoV2(), 'Expecting an access token');
 });
@@ -11,7 +13,7 @@ test('API construct has `config` property', t => {
 });
 
 test('Invalid API response', async t => {
-	await t.throws(API.getAction('aaa'), /Cannot read property/);
+	await t.throws(API.getAction(FAKEID), /Cannot read property/);
 });
 
 test('Account.getUser()', async t => {
@@ -19,11 +21,11 @@ test('Account.getUser()', async t => {
 });
 
 test('Account.listActions()', async t => {
-	const res = await t.notThrows(API.listActions());
-	// fetch an action, if had any
-	if (res.length) {
-		await t.notThrows(API.getAction(res[0].id), 'Account.getAction(id)');
-	}
+	await t.notThrows(API.listActions());
+});
+
+test('Account.getAction(id)', async t => {
+	await t.throws(API.getAction(FAKEID));
 });
 
 test('Domain.listDomains()', async t => {
