2ce8bf6e6da378815c95c3279857ba63c3ba4e6d
luke <luke.edwards05@gmail.com>
2016-05-08 13:56:53 -0700

    account & domain tests

--- test/index.js
+++ test/index.js
@@ -10,7 +10,26 @@ test('API construct has `config` property', t => {
 	t.truthy(API.config, 'exists');
 });
 
-test('Account.getUser', async t => {
-	const res = await t.notThrows(API.getUser());
-	t.truthy(res);
+test('Invalid API response', async t => {
+	await t.throws(API.getAction('aaa'), /Cannot read property/);
+});
+
+test('Account.getUser()', async t => {
+	await t.notThrows(API.getUser());
+});
+
+test('Account.listActions()', async t => {
+	const res = await t.notThrows(API.listActions());
+	// fetch an action, if had any
+	if (res.length) {
+		await t.notThrows(API.getAction(res[0].id), 'Account.getAction(id)');
+	}
+});
+
+test('Domain.listDomains()', async t => {
+	await t.notThrows(API.listDomains());
+	// // fetch an action, if had any
+	// if (res.length) {
+	// 	await t.notThrows(API.getAction(res[0].id), 'Domain.getAction(id)');
+	// }
 });
